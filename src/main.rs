fc4b99d90388e5fb2ffa608e308b7494cba8fcc4
Muhammad Saheed <muhammad.saheed.iam@gmail.com>
2024-05-05 23:42:06 +0530

    Grab the keyboard input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 use clap::{Parser, Subcommand};
+use x11rb::{
+    connection::Connection,
+    protocol::{
+        xproto::{ConnectionExt, GrabMode, GrabStatus},
+        Event,
+    },
+};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -13,6 +20,46 @@ enum Commands {
     Normal,
 }
 
-fn main() {
-    let _ = Cli::parse();
+fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Normal => {
+            let (conn, screen_num) = x11rb::connect(None)?;
+            let screen = &conn.setup().roots[screen_num];
+
+            let reply = conn
+                .grab_keyboard(
+                    true,
+                    screen.root,
+                    x11rb::CURRENT_TIME,
+                    GrabMode::ASYNC,
+                    GrabMode::ASYNC,
+                )?
+                .reply()?;
+            if reply.status == GrabStatus::SUCCESS {
+                println!("Grabbed keyboard successfully!");
+            } else {
+                println!("Failed to Grab keyboard!");
+            }
+
+            loop {
+                let event = conn.wait_for_event()?;
+
+                match event {
+                    Event::KeyRelease(event) => match event.detail {
+                        9 => break,
+                        key => {
+                            println!("Released key: {key:?}");
+                        }
+                    },
+                    _ => {}
+                }
+            }
+
+            conn.ungrab_keyboard(x11rb::CURRENT_TIME)?;
+        }
+    };
+
+    Ok(())
 }
