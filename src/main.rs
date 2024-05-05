5a1e7262b083a796ed7e3b45fa17317ad9ea1d33
Muhammad Saheed <muhammad.saheed.iam@gmail.com>
2024-05-05 15:28:50 +0530

    Scaffold the CLI

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Run in normal mode.
+    Normal,
+}
+
+fn main() {
+    let _ = Cli::parse();
+}
